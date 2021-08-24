5426e29bb3a87a37b798858f388444b8b0091c50
Samuel <oiranca@gmail.com>
2021-08-24 16:30:55 +0100

    fix prettier and eslint

--- final-agile/.eslintrc.js
+++ final-agile/.eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  // For commons project (.ts and .tsx files)
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    rules: {
+    'import/resolver': {
+      typescript: {
+        project: `${__dirname}/tsconfig.json`
+      }
+    }
+  },
+  overrides: [
+    {
+      files: ['src/**/*.ts?(x)'],
+      excludedFiles: ['src/react-app-env.d.ts'],
+      extends: ['plugin:@typescript-eslint/recommended', 'airbnb-typescript-prettier'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname
+      },
+      rules: {
+        'react/jsx-curly-brace-presence': 0,
+        'react/jsx-boolean-value':['off'],
         'no-param-reassign': 'off',
         'react/require-default-props': 'off',
         'react/jsx-props-no-spreading': ['off'],
@@ -30,11 +32,19 @@ module.exports = {
         '@typescript-eslint/explicit-module-boundary-types': ['off'],
         'import/prefer-default-export': ['off'],
         'react/jsx-filename-extension': [
-            1,
-            {
-                extensions: ['.ts', '.tsx']
-            }
+          1,
+          {
+            extensions: ['.ts', '.tsx']
+          }
         ],
         'no-shadow': ['off']
+      }
+    },
+    {
+      files: ['src/**/routing/**/*.ts?(x)'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
     }
+  ]
 };

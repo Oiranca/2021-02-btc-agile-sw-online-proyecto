772909c6be87f3f58edbb95fd15f777ce25811b9
Samuel <oiranca@gmail.com>
2021-08-25 13:47:04 +0100

    modified DataAndResultArea.tsx, CalculateForm.tsx components and introduce logic

--- final-agile/src/components/molecules/DataAndResultArea/DataAndResultArea.tsx
+++ final-agile/src/components/molecules/DataAndResultArea/DataAndResultArea.tsx
@@ -1,10 +1,73 @@
 import * as React from 'react';
 import './dataAndResultArea.scss';
-import { FieldInput } from '../../atoms/FieldInput/FieldInput';
+import { NumberArea } from '../NumberArea/NumberArea';
+import { MathOperation } from '../MathOperation/MathOperation';
 
-export const DataAndResultArea: React.FC = () => (
-  <section className={'DataAndResultArea'}>
-    <FieldInput name={'result'} type={'text'} readOnly />
-    <FieldInput name={'data'} type={'text'} readOnly={false} />
-  </section>
-);
+export const DataAndResultArea: React.FC = () => {
+  const [value, setValue] = React.useState<string[]>([]);
+  const [result, setResult] = React.useState<string>('');
+  const isANumber = RegExp(/\d/);
+
+  const operation = (previousNumber: string, lastNumber: string, operator: string) => {
+    switch (operator) {
+      case '+':
+        return (Number(previousNumber) + Number(lastNumber)).toString();
+      case '-':
+        return (Number(previousNumber) - Number(lastNumber)).toString();
+      case '*':
+        return (Number(previousNumber) * Number(lastNumber)).toString();
+      default:
+        return '';
+    }
+  };
+
+  const calculate = (values: string[]) => {
+    let lastNumber = '';
+    let previousNumber = '';
+    let operator = '';
+    for (const element of values) {
+      if (isANumber.test(element)) {
+        lastNumber += element;
+      } else if (!isANumber.test(element)) {
+        if (element === '=') {
+          lastNumber = operation(previousNumber, lastNumber, operator);
+        } else if (operator === '') {
+          previousNumber = lastNumber;
+          lastNumber = '';
+          operator = element;
+        } else {
+          previousNumber = operation(previousNumber, lastNumber, operator);
+          lastNumber = '';
+        }
+      }
+    }
+    return lastNumber;
+  };
+
+  const handleOnClick = (e: string) => {
+    setValue([...value, e]);
+  };
+
+  React.useEffect(() => {
+    if (value.includes('=')) {
+      setResult(calculate(value)!);
+      setValue(['']);
+    }
+  }, [value]);
+  return (
+    <div className={'DataAndResultArea'}>
+      <div className={'resultArea'}>
+        <div className={'result'}>{result}</div>
+        <div className={'data'}>{value}</div>
+      </div>
+      <div className={'operation row'}>
+        <div className={'digit col'}>
+          <NumberArea value={handleOnClick} />
+        </div>
+        <div className={'operator col'}>
+          <MathOperation value={handleOnClick} />
+        </div>
+      </div>
+    </div>
+  );
+};
